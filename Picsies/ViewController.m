361377a31e8838ce43cca79c73684653a0d1963a
tamaragevorgyan <tamarik@picsart.com>
2016-02-19 16:26:01 +0400

    some changes

--- Picsies/ViewController.m
+++ Picsies/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import <FBSDKMessengerShareKit/FBSDKMessengerShareKit.h>
 
 @interface ViewController ()
 
@@ -16,12 +17,26 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    UIButton *button = [FBSDKMessengerShareButton rectangularButtonWithStyle:FBSDKMessengerShareButtonStyleBlue];
+    [button addTarget:self action:@selector(sendAction) forControlEvents:UIControlEventTouchUpInside];
+    [button setTitle:@"Send" forState:UIControlStateNormal];
+    [self.view addSubview:button];
+    button.center = self.view.center;
+    
+    //[FBSDKMessengerSharer messengerPlatformCapabilities] & FBSDKMessengerPlatformCapabilityImage)
+    //NSLog(@"Error - Messenger platform capabilities don't include image sharing");
+}
+
+-(void)sendAction {
+    FBSDKMessengerShareOptions *options = [[FBSDKMessengerShareOptions alloc] init];
+    options.renderAsSticker = YES;
+    
+    [FBSDKMessengerSharer shareImage:[UIImage imageNamed:@"test_icon"] withOptions:options];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 @end
