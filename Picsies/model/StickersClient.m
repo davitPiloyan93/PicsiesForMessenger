6330c77356767361aa66d8f74ac269572ab63e19
davitpiloyan93 <davit.piloyan@picsart.com>
2016-03-25 17:18:52 +0400

    added json,for cached items

--- Picsies/model/StickersClient.m
+++ Picsies/model/StickersClient.m
@@ -9,58 +9,22 @@
 #import "StickersClient.h"
 #import "Sticker.h"
 
-#define STICKERS_URL @"http://api.picsart.com/apps.json?app=com.picsart.studio&market=apple&type=apps&offet=0&limit=-1"
-
-@interface StickersClient ()
-
-
-@end
-
 @implementation StickersClient
 
 - (void)stickersWithComplitionBlock:(void (^)(NSArray * items))complitionBlock {
-    if ([[NSUserDefaults standardUserDefaults] objectForKey:@"stickers"]) {
-        NSMutableArray *items = [[NSMutableArray alloc] init];
-        for (NSDictionary *item in [[NSUserDefaults standardUserDefaults] objectForKey:@"stickers"]) {
-            Sticker *sticker = [[Sticker alloc] initWithDictionary:item];
-            if ([sticker.shop_item_name_prefix isEqual:@"i_clipart"]) {
-                [items addObject:sticker];
-            }
-        }
-        complitionBlock(items);
-    } else {
-        [[[NSURLSession sharedSession] dataTaskWithURL:[NSURL URLWithString:STICKERS_URL] completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
-            dispatch_async(dispatch_get_main_queue(), ^{
-                if (error) {
-                    complitionBlock(nil);
-                } else {
-                    NSError *jsonError;
-                    NSDictionary *dictionary = [NSJSONSerialization JSONObjectWithData:data
-                                                                               options:0 error:&jsonError];
-                    if (jsonError) {
-                        complitionBlock(nil);
-                        return;
-                    }
-                    
-                    NSMutableArray *arrayOfDictinaries = [[NSMutableArray alloc] init];
-                    NSMutableArray *items = [[NSMutableArray alloc] init];
-                    for (NSDictionary *item in dictionary[@"response"]) {
-                        Sticker *sticker = [[Sticker alloc] initWithDictionary:item];
-                        if ([sticker hasTag:@"Picsies"] && [sticker.shop_item_name_prefix isEqual:@"i_clipart"]) {
-                        
-                            [arrayOfDictinaries addObject:item];
-                            [items addObject:sticker];
-                        }
-                    }
-                    complitionBlock(items);
-                    
-                    [[NSUserDefaults standardUserDefaults] setObject:arrayOfDictinaries forKey:@"stickers"];
-                    [[NSUserDefaults standardUserDefaults] synchronize];
-                }
-            });
-        }] resume];
-    }
-}
+    NSMutableArray *items = [[NSMutableArray alloc] init];
+    NSString *filePath = [[NSBundle mainBundle]pathForResource:@"picsies" ofType:@"json"];
+    NSData *jsonData = [NSData dataWithContentsOfFile:filePath];
+    NSDictionary *itemsInfoFromJson = [NSJSONSerialization JSONObjectWithData:jsonData
+                                                                      options:0
+                                                                        error:nil];
 
+    for (NSDictionary *item in itemsInfoFromJson) {
+        Sticker *sticker = [[Sticker alloc] initWithDictionary:item];
+        [items addObject:sticker];
+    }
+    complitionBlock(items);
+    
+}
 
 @end
